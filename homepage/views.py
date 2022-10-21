e2d7a591b3c15280ac138e81591b9823fc60f23b
Osman Közen <50768807+OsmanKozen@users.noreply.github.com>
2022-10-21 10:49:17 +0300

    Add files via upload

---
+++ homepage/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponse
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from weeklyreport.models import Users, Services
+
+# Create your views here.
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log the user in
+            user = form.get_user()
+            login(request, user)
+            return redirect("homepage:dashboard")
+    # else:
+    #     form = AuthenticationForm()
+
+    return render(request, "homepage/login.html", {'form': form})
+
+
+@login_required()
+def dashboard_view(request):
+    team = Users.objects.filter(username=request.user).values_list(
+        'teamname', flat=True)[0]
+    services = Services.objects.filter(teamname=team).order_by('servicename')
+
+    content = {
+        "user": request.user,
+        "team": team,
+        "services": services
+    }
+
+    return render(request, "homepage/dashboard.html", content)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
